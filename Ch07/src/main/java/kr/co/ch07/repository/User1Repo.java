f661498704478a9062771c489af6ef2566891e4c
realKimJaeJUN <nagandr1113@gmail.com>
2023-01-11 18:00:57 +0900

    Ch07 예문 추가

--- Ch07/src/main/java/kr/co/ch07/repository/User1Repo.java
+++ Ch07/src/main/java/kr/co/ch07/repository/User1Repo.java
@@ -1,11 +1,49 @@
 package kr.co.ch07.repository;
 
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import kr.co.ch07.vo.User1VO;
 
 @Repository
 public interface User1Repo extends JpaRepository<User1VO, String>{
-
+	// JPA Query Method(쿼리메서드)
+	public User1VO findUser1VOByUid(String uid);
+	public List<User1VO> findUser1VOByName(String name);
+	public List<User1VO> findUser1VOByNameNot(String name);
+	
+	public User1VO findUser1VOByUidAndName(String uid, String name);
+	public List<User1VO> findUser1VOByUidOrName(String uid, String name);
+	
+	public List<User1VO> findUser1VOByAgeGreaterThan(int age);
+	public List<User1VO> findUser1VOByAgeGreaterThanEqual(int age);
+	public List<User1VO> findUser1VOByAgeLessThan(int age);
+	public List<User1VO> findUser1VOByAgeLessThanEqual(int age);
+	
+	public List<User1VO> findUser1VOByNameLike(String name); // name 앞 또는 뒤에 % 명시 필요
+	public List<User1VO> findUser1VOByNameContains(String name);
+	public List<User1VO> findUser1VOByNameStartsWith(String name);
+	public List<User1VO> findUser1VOByNameEndsWith(String name);
+	
+	public List<User1VO> findUser1VOByOrderByName();
+	public List<User1VO> findUser1VOByOrderByAgeAsc();
+	public List<User1VO> findUser1VOByOrderByAgeDesc();
+	public List<User1VO> findUser1VOByAgeGreaterThanOrderByAgeDesc(int age);
+	
+	public int countUser1VOByUid(String uid);
+	public int countUser1VOByName(String name);
+	
+	// JPQL (JPA Query Language)
+	@Query("select u1 from User1VO as u1 where age < 30")
+	public List<User1VO> selectUserUnderAge30();
+	
+	@Query("select u1 from User1VO as u1 where u1.name = ?1")
+	public List<User1VO> selectUserByName(String name);
+	
+	@Query("select u1 from User1VO as u1 where u1.name = :name")
+	public List<User1VO> selectUserByNameWithParam(@Param("name") String name);
 }
