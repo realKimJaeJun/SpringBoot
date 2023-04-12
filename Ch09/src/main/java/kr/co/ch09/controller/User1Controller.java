9a29a85f9f0f6972960bfa0056ae68ff758763e2
realKimJaeJUN <nagandr1113@gmail.com>
2023-04-12 18:15:08 +0900

    수정사항

--- Ch09/src/main/java/kr/co/ch09/controller/User1Controller.java
+++ Ch09/src/main/java/kr/co/ch09/controller/User1Controller.java
@@ -6,11 +6,13 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import kr.co.ch09.service.User1Service;
@@ -23,21 +25,29 @@ public class User1Controller {
 	private User1Service service;
 	
 	@ResponseBody
-	@GetMapping("/user1")
+	@CrossOrigin(origins = "*")
+	@GetMapping("/user1s")
 	public List<User1VO> list1() {
 		List<User1VO> users = service.selectUser1s();
 		return users;
 	}
 	
 	@ResponseBody
-	@GetMapping("/user1/{id}")
-	public User1VO list2(@PathVariable("id") String uid) {
+	@GetMapping("/user1")
+	public User1VO list2(String uid) {
 		return service.selectUser1(uid);
 	}
 	
 	@ResponseBody
+	@GetMapping("/user1/{id}")
+	public User1VO list3(@PathVariable("id") String uid) {
+		return service.selectUser1(uid);
+	}
+	
+	@ResponseBody
+	@CrossOrigin(origins = "*")
 	@PostMapping("/user1")
-	public Map<String, Integer> register(User1VO vo) {		
+	public Map<String, Integer> register(@RequestBody User1VO vo) {		
 		int result = service.insertUser1(vo);
 		
 		Map<String, Integer> resultMap = new HashMap<>();
@@ -47,24 +57,21 @@ public class User1Controller {
 	}
 	
 	@ResponseBody
+	@CrossOrigin(origins = "*")
 	@PutMapping("/user1")
-	public Map<String, Integer> modify(User1VO vo) {
+	public List<User1VO> modify(@RequestBody User1VO vo) {
 		int result = service.updateUser1(vo);
 		
-		Map<String, Integer> resultMap = new HashMap<>();
-		resultMap.put("result", result);
-		
-		return resultMap;
+		return service.selectUser1s();
 	}
 	
 	@ResponseBody
+	@CrossOrigin(origins = "*")
 	@DeleteMapping("/user1/{id}")
-	public Map<String, Integer> delete(@PathVariable("id") String uid) {
+	public List<User1VO> delete(@PathVariable("id") String uid) {
 		int result = service.deleteUser1(uid);
-		Map<String, Integer> resultMap = new HashMap<>();
-		resultMap.put("result", result);
-		
-		return resultMap;
+
+		return service.selectUser1s();
 	}
 	
 }
